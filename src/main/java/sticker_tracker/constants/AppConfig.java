1f74db616a07d77ea1b8d5167183e1ee4d6809af
ruymon <ruystfrancis@gmail.com>
2026-06-01 20:58:19 -0300

    feat(config): Add app defaults

---
+++ src/main/java/sticker_tracker/constants/AppConfig.java
@@ -0,0 +1,11 @@
+package sticker_tracker.constants;
+
+public final class AppConfig {
+
+    private AppConfig() {}
+
+    public static final String APP_NAME = "Cola Aí";
+    public static final String APP_TITLE = APP_NAME;
+    public static final String APP_SLOGAN = "A cola que o professor não pode reprovar.";
+    public static final String DEFAULT_COLLECTION_ID = "00000000-0000-0000-0000-000000000001";
+}
